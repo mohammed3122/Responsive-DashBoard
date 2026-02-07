71db971700d2ae02a8af44e2f2d3be87b8e53254
Mohammad Awd <111813467+mohammed3122@users.noreply.github.com>
2026-02-08 03:41:26 +0400

    add styles , images and font done

---
+++ lib/utils/app_images.dart
@@ -0,0 +1,32 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+
+  static const String imagesAvatar1 = "assets/images/avatar1.svg";
+
+  static const String imagesAvatar2 = "assets/images/avatar2.svg";
+
+  static const String imagesAvatar3 = "assets/images/avatar3.svg";
+
+  static const String imagesCategory = "assets/images/category.svg";
+
+  static const String imagesChart = "assets/images/chart.svg";
+
+  static const String imagesConvertCard = "assets/images/convert-card.svg";
+
+  static const String imagesGraph = "assets/images/graph.svg";
+
+  static const String imagesInCome = "assets/images/in_come.svg";
+
+  static const String imagesLogout = "assets/images/logout.svg";
+
+  static const String imagesMaskgroup = "assets/images/mask_group.svg";
+
+  static const String imagesMoneys = "assets/images/moneys.svg";
+
+  static const String imagesOutCome = "assets/images/out_come.svg";
+
+  static const String imagesSetting = "assets/images/setting.svg";
+
+  static const String imagesWallet = "assets/images/wallet.svg";
+}
